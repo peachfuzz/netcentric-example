375f73431def72896eaa5e3e733392fcedb9226b
Hector <hectordm65@gmail.com>
2019-05-06 12:00:10 -0700

    App is now using props and state

--- src/App.js
+++ src/App.js
@@ -2,20 +2,29 @@ import React from "react";
 import "./App.css";
 import Friends from "./Components/Friends/Friends";
 
-function App() {
-  return (
-    <div className="container justify-content-center text-center">
-      <h1>An Example in React</h1>
-      <table className="table table-bordered table-hover">
-        <tr>
-          <th scope="col">First Name</th>
-          <th scope="col">Last Name</th>
-          <th scope="col">Phone</th>
-        </tr>
-        <Friends />
-      </table>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      friends: require("./Components/Friends/friends.json").friends
+    };
+  }
+
+  render() {
+    return (
+      <div className="container justify-content-center text-center">
+        <h1>An Example in React</h1>
+        <table className="table table-bordered table-hover">
+          <tr>
+            <th scope="col">First Name</th>
+            <th scope="col">Last Name</th>
+            <th scope="col">Phone</th>
+          </tr>
+          <Friends friends={this.state.friends} />
+        </table>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Components/Friends/Friends.js
+++ src/Components/Friends/Friends.js
@@ -2,14 +2,8 @@ import React from "react";
 // var friends = require("./friends.json");
 
 class Friends extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      data: require("./friends.json")
-    };
-  }
   render() {
-    return this.state.data.friends.map(friend => {
+    return this.props.friends.map(friend => {
       return (
         <tr>
           <td>{friend.firstName}</td>
