d522f49c8be5dabcf19ef453e953563aeadb8347
Hector <hectordm65@gmail.com>
2019-05-06 11:55:35 -0700

    Added code to break app into pieces

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import Footer from "./Components/Footer";
-import TodoApp from "./Components/ToDoApp";
+import Friends from "./Components/Friends/Friends";
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Footer />
-        <TodoApp />
-      </div>
+    <div className="container justify-content-center text-center">
+      <h1>An Example in React</h1>
+      <table className="table table-bordered table-hover">
+        <tr>
+          <th scope="col">First Name</th>
+          <th scope="col">Last Name</th>
+          <th scope="col">Phone</th>
+        </tr>
+        <Friends />
+      </table>
     </div>
   );
 }
