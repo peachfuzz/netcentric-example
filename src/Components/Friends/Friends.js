fdf1a1824f9831e206e14dc25d8730e9e10a54cf
Hector <hectordm65@gmail.com>
2019-05-11 19:11:35 -0700

    Added a key value to each item

--- src/Components/Friends/Friends.js
+++ src/Components/Friends/Friends.js
@@ -2,17 +2,17 @@ import React from "react";
 // var friends = require("./friends.json");
 
 class Friends extends React.Component {
-  render() {
-    return this.props.friends.map(friend => {
-      return (
-        <tr>
-          <td>{friend.firstName}</td>
-          <td>{friend.lastName}</td>
-          <td>{friend.phone}</td>
-        </tr>
-      );
-    });
-  }
+    render() {
+        return this.props.friends.map((friend, index) => {
+            return (
+                <tr key={index}>
+                    <td>{friend.firstName}</td>
+                    <td>{friend.lastName}</td>
+                    <td>{friend.phone}</td>
+                </tr>
+            );
+        });
+    }
 }
 
 export default Friends;
