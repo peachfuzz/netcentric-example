e984973d5dbecab2f05f82134501eee1913ca1a3
Hector <hectordm65@gmail.com>
2019-05-11 19:11:16 -0700

    Made the components more modular

--- src/App.js
+++ src/App.js
@@ -1,42 +1,29 @@
 import React from "react";
 import "./App.css";
-import Friends from "./Components/Friends/Friends";
+import FriendsTable from "./Components/Friends/FriendsTable";
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      friends: require("./Components/Friends/friends.json").friends
-    };
-  }
-
-  render() {
-    return (
-      <div className="container justify-content-center text-center">
-        <h1>An Example in React</h1>
-        <table className="table table-bordered table-hover">
-          <tr>
-            <th scope="col">First Name</th>
-            <th scope="col">Last Name</th>
-            <th scope="col">Phone</th>
-          </tr>
-          <Friends friends={this.state.friends} />
-        </table>
-        <small>
-          <code>
-            Learn more about react{" "}
-            <a
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              here
-            </a>
-          </code>
-        </small>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="container justify-content-center text-center">
+                <h1>An Example in React</h1>
+                {/* created the top of the head */}
+                <FriendsTable />
+                <small>
+                    <code>
+                        Learn more about react{" "}
+                        <a
+                            href="https://reactjs.org"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            here
+                        </a>
+                    </code>
+                </small>
+            </div>
+        );
+    }
 }
 
 export default App;
